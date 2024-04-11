e24927c64956740ed7ce0b94cf7dadb640c9bccd
Anderson0421 <jelow0413@gmail.com>
2024-04-11 13:15:02 -0500

    asdsa

--- app/blog/category/layout.jsx
+++ app/blog/category/layout.jsx
@@ -1,4 +1,8 @@
-export default function CategoryLayout({children}){
+
+export const metadata = {
+    title: 'Category Page',
+}
+export default function CategoryLayout({ children }) {
     return (
         <div>
             <h1>Category Layout</h1>
--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,5 +1,8 @@
-export default function BlogPage(){
-    return(
+export const metadata = {
+    title: 'Blog Page',
+}
+export default function BlogPage() {
+    return (
         <>
             <h1 className="text-4xl font-bold">Hello, My Blog!</h1>
         </>
